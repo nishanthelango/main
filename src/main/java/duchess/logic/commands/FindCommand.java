75151218c97c6857bc443aa009892404ba8ea5e3
nishanthelango <nishanthelango@yahoo.com.sg>
2019-10-06 20:37:49 +0800

    Added longest common subsequence for better search

--- src/main/java/duchess/logic/commands/FindCommand.java
+++ src/main/java/duchess/logic/commands/FindCommand.java
@@ -12,6 +12,27 @@ import java.util.stream.Collectors;
 public class FindCommand extends Command {
     private List<String> words;
 
+    /**
+     * Returns the length of the longest common subsequence of 2 Strings.
+     *
+     * @param a the first String
+     * @param b the second String
+     * @return the length of the longest common subsequence
+     */
+    private int lcs(String a, String b) {
+        int[][] table = new int[a.length() + 1][b.length() + 1];
+        for (int i = 1; i < table.length; i++) {
+            for (int j = 1; j < table[0].length; j++) {
+                if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                    table[i][j] = table[i - 1][j - 1] + 1;
+                } else {
+                    table[i][j] = Math.max(table[i - 1][j], table[i][j - 1]);
+                }
+            }
+        }
+        return table[a.length()][b.length()];
+    }
+
     public FindCommand(List<String> words) {
         this.words = words;
     }
@@ -22,7 +43,9 @@ public class FindCommand extends Command {
             String searchTerm = String.join(" ", words.subList(0, words.size()));
             List<Task> filteredTasks =
                     store.getTaskList().stream()
-                            .filter(task -> task.containsKeyword(searchTerm))
+                            .filter(task -> lcs(task.getDescription().replaceAll(" ",""),
+                                    searchTerm.replaceAll(" ",""))
+                                    >= searchTerm.replaceAll(" ", "").length())
                             .collect(Collectors.toList());
             if (filteredTasks.size() > 0) {
                 ui.showSearchResult(filteredTasks);
